a5bca42ef1756869a27d79918da97300e64dd868
Jeff Hykin <jeff.hykin@gmail.com>
2025-02-24 17:48:04 -0600

    unify interface

--- src/serial_port.ts
+++ src/serial_port.ts
@@ -1,19 +1,31 @@
-import { SerialOpenOptions } from "./common/serial_port.ts";
+import { SerialOpenOptions } from "./common/serial_port.ts"
+import { SerialPortInfo, SerialPort } from "./common/web_serial.ts";
 // import { getPortsDarwin } from "./darwin/enumerate.ts";
-import { getPortsWin, SerialPortWin } from "./windows/mod.ts";
+import { getPortsWin, SerialPortWin } from "./windows/mod.ts"
+import { getPortsDarwin } from "./darwin/enumerate.ts"
+import { SerialPortDarwin } from "./darwin/serial_port.ts"
+import { getPortsLinux } from "./linux/enumerate.ts"
 
-export function getPorts() {
-  if (Deno.build.os === "windows") {
-    return getPortsWin();
-  } else {
-    throw new Error(`Unsupported OS: ${Deno.build.os}`);
-  }
+export function getPorts() : SerialPortInfo[] {
+    if (Deno.build.os === "windows") {
+        return getPortsWin()
+    } else if (Deno.build.os === "darwin") {
+        return getPortsDarwin()
+    } else if (Deno.build.os === "linux") {
+        return getPortsLinux()
+    } else {
+        throw new Error(`Unsupported OS: ${Deno.build.os}`)
+    }
 }
 
-export function open(options: SerialOpenOptions) {
-  if (Deno.build.os === "windows") {
-    return new SerialPortWin(options);
-  } else {
-    throw new Error(`Unsupported OS: ${Deno.build.os}`);
-  }
+export async function open(options: SerialOpenOptions) : Promise<SerialPort> {
+    if (Deno.build.os === "windows") {
+        return new SerialPortWin(options)
+    } else if (Deno.build.os === "darwin") {
+        return new SerialPortDarwin(options.name).open(options)
+    } else if (Deno.build.os === "linux") {
+        return new SerialPort(name).open(options)
+    } else {
+        throw new Error(`Unsupported OS: ${Deno.build.os}`)
+    }
 }
