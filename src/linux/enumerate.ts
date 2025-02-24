92717dc356ce226aa9ce9167b27e13e46df0bc87
Jeff Hykin <jeff.hykin@gmail.com>
2025-02-24 17:56:07 -0600

    improve enumerate a bit

--- src/linux/enumerate.ts
+++ src/linux/enumerate.ts
@@ -3,7 +3,7 @@ import { FileSystem, glob } from "https://deno.land/x/quickr@0.7.4/main/file_sys
 export async function getPortsLinux() {
   let ports = []
   for (const each of Deno.readDirSync("/dev/")) {
-    if (each.name.startsWith("tty")) {
+    if (!each.isDirectory && each.name.startsWith("tty")) {
       ports.push({name:"/dev/" + each.name})
     }
   }
